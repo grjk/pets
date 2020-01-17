e6ed1fd3c079ee07e0e8e552b56cb43daa483024
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-01-17 13:20:53 -0500

    index.php created, F3 running

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require("vendor/autoload.php");
+
+// Instantiate F3
+$f3 = Base::instance();
+
+// Defining a default route
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo "Sup";
+});
+
+// Run F3
+$f3->run();
+
+?>
